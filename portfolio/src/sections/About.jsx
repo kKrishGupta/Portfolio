ee7b8ec0c28679e360ac47e556b5b7663341c1d9
kKrishGupta <kg20060220@gmail.com>
2026-03-07 01:42:48 +0530

    annimated pulse  fixed

--- portfolio/src/sections/About.jsx
+++ portfolio/src/sections/About.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
-const About = () => {
+export default function About() {
+  const glows = [
+    "-top-10 -left-10 w-90 h-90 opacity-20 blur-3xl ",
+    "-bottom-0 -right-10 w-105 h-105 opacity-15 blur-35 delay-300",
+    "top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-75 h-75 opacity-10 blur-[120px] ",
+  ]
   return (
-    <div className='w-full h-screen'>
-        About
-    </div>
+    <section id="about"
+    className="min-h-screen w-full flex items-center justify-center relative bg-black text-white overflow-hidden">
+      
+      <div className="absolute inset-0 pointer-events-none">
+        {glows.map((c,i) =>(
+          <div key={i} className={`absolute rounded-full  bg-linear-to-r from-[#302b63] via-[#00bf8f] to-[#1cd8d2] animate-pulse ${c}`}/>
+        ))}
+      </div>
+
+    </section>
   )
 }
-
-export default About
--- portfolio/src/sections/Home.jsx
+++ portfolio/src/sections/Home.jsx
@@ -81,7 +81,7 @@ transition={{duration:0.5, delay:0.1, ease:"easeInOut"}}>
 initial={{opacity:0, y:40}}
 animate={{opacity:1, y:0}}
 transition={{duration:1}}>
-  Hello I'm 
+  Hello, I'm 
   <br />
   <span className="text-white font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl lg:whitespace-nowrap">
     Krish Gupta
